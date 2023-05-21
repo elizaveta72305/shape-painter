cbea7e08794a26ab2dbe533869bc0c0ef4a1f7b0
unknown <elizaveta72305@gmail.com>
2023-05-21 15:32:47 +0300

    add 2 stage

--- src/app/shape/shape.component.ts
+++ src/app/shape/shape.component.ts
@@ -1,14 +1,89 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+interface Shape {
+  shapeId: number,
+  lines: Line[],
+  selected: boolean,
+  color: string
+}
+
+
 @Component({
   selector: 'app-shape',
   templateUrl: './shape.component.html',
   styleUrls: ['./shape.component.css']
 })
-export class ShapeComponent {
-    shapeId = 'myShape';
-    selectedColor = '';
 
-    changeColor(color: string) {
-      this.selectedColor = color;
+export class ShapeComponent {
+  shapeId: number;
+  selectedShape: Shape | null = null;
+  colors: string[] = ['black', 'red', 'blue', 'green'];
+  basicColor = 'black';
+
+  shapes: Shape[] = [
+    {
+    shapeId: 1,
+    lines: [
+      { from: { x: 0, y: 0 }, to: { x: 150, y: 0 } },
+      { from: { x: 150, y: 0 }, to: { x: 150, y: 150 } },
+      { from: { x: 150, y: 150 }, to: { x: 0, y: 150 } },
+      { from: { x: 0, y: 150 }, to: { x: 0, y: 0 } }
+    ],
+    selected: false,
+    color: 'black'
+    },
+    {
+    shapeId: 2,
+    lines: [
+      { from: { x: 200, y: 0 }, to: { x: 350, y: 0 } },
+      { from: { x: 350, y: 0 }, to: { x: 350, y: 150 } },
+      { from: { x: 350, y: 150 }, to: { x: 200, y: 150 } },
+      { from: { x: 200, y: 150 }, to: { x: 200, y: 0 } }
+    ],
+    selected: false,
+    color: 'black'
+  }
+  ];
+
+  constructor() {
+    this.shapeId = this.generateShapeId();
+  }
+
+  // selectShape(shape: Shape): void {
+  //   this.selectedShape = shape;
+  // }
+
+  // isShapeSelected(shape: Shape): boolean {
+  //   return this.selectedShape === shape;
+  // }
+
+  changeShapeColor(newColor: string): void {
+    const selectedShape = this.shapes.find(shape => shape.selected);
+    if (selectedShape) {
+      selectedShape.color = newColor;
     }
+  }
+
+  onShapeClick(clickedShape: Shape): void {
+    this.shapes.forEach(shape => {
+      if (shape === clickedShape) {
+        shape.selected = true;
+      } else {
+        shape.selected = false;
+      }
+    });
+  }
+  private generateShapeId(): number {
+    return Math.floor(Math.random() * 1000);
+  }
 }
+
+interface Line {
+  from: Point;
+  to: Point;
+}
+
+interface Point {
+  x: number;
+  y: number;
+}
+
